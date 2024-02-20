7e811393f599ca101e5a1a12b5a95380ec7b9925
MScott-Dev <MScott0199@gmail.com>
2024-02-20 16:37:27 -0500

    Added fetch method for openweatherapi

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "fe95411da9msh29bc1f391fd07efp1e8444jsn9aa6c5f27ce8",
+    "X-RapidAPI-Key": ``,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
